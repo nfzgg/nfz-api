7fcd8a7b332bf4fee2a81944481b751860337ee1
Marios Isaakidis <prometheas@autistici.org>
2022-09-25 18:40:18 +0300

    Move abi files under /assets/

---
+++ public/assets/abiNFZFactory.js
@@ -0,0 +1,36 @@
+abiNFZFactory = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "symbol",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "infusedCollection",
+				"type": "address"
+			}
+		],
+		"name": "infuse",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
--- public/javascripts/nfz.js
+++ public/javascripts/nfz.js
@@ -50,39 +50,3 @@ async function infuseCollection() {
 }
 
 
-abiNFZFactory = [
-	{
-		"inputs": [],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "name",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "symbol",
-				"type": "string"
-			},
-			{
-				"internalType": "address",
-				"name": "infusedCollection",
-				"type": "address"
-			}
-		],
-		"name": "infuse",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
-]
