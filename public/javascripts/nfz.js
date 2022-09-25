105eda24eb3b7df68dfb6e247cf6451cc14141a6
Marios Isaakidis <prometheas@autistici.org>
2022-09-25 18:07:25 +0300

    Infuse NFT collections via the website

--- public/javascripts/nfz.js
+++ public/javascripts/nfz.js
@@ -1,10 +1,15 @@
+import('./web3.min.js');
+
 window.addEventListener('load', function () {
     addListeners();
   })
 
 if (typeof window.ethereum !== 'undefined') {
     console.log('MetaMask is installed!');
+    window.web3 = new Web3(window.ethereum);
 }
+
+const addrNFZFactory = '0x4c4af5e192db9d02c65a1486f6f34c30eafc55b0';
 
 var accounts;
 var ethereumButton;
@@ -29,8 +34,55 @@ async function getAccount() {
     const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
     const account = accounts[0];
     showAccount.innerHTML = account;
+	return account;
 }
 
 async function infuseCollection() {
-    console.log('Infuse');
+    NFTCollection = document.getElementById('NFTCollection').value;
+    NFZName = document.getElementById('NFZName').value;
+
+    const NFZFactory = await new window.web3.eth.Contract(abiNFZFactory, addrNFZFactory);
+    const account = await getAccount();
+
+    const deployed = await NFZFactory.methods.infuse(NFZName, 'NFZ', NFTCollection).send({from: account});
+	console.log('NFZ deployed\n' + JSON.stringify(deployed));
+    alert('NFZ deployed at ' + deployed.events[0].address);
 }
+
+
+abiNFZFactory = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "symbol",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "infusedCollection",
+				"type": "address"
+			}
+		],
+		"name": "infuse",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
