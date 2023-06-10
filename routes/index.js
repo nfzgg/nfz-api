22ca0d54c4fe905cd0670187761fcfbbeb31c223
Marios Isaakidis <prometheas@autistici.org>
2023-06-10 22:19:51 +0200

    Move demo page; Add favicon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NFZ.gg' });
+  res.render('index', { title: 'NFZ Transferable Utilities' });
+});
+
+/* GET home page. */
+router.get('/demo', function(req, res, next) {
+  res.render('demo', { title: 'NFZ Demo' });
 });
 
 module.exports = router;
